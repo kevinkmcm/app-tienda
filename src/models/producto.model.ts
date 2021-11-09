eebd6f6734d080ca959da234f23718b37808173f
kevin mauricio <kevinkmcm123@gmail.com>
2021-11-09 13:03:52 -0500

    vercion 1 de app pedidos

---
+++ src/models/producto.model.ts
@@ -0,0 +1,49 @@
+import {Entity, model, property, hasOne} from '@loopback/repository';
+import {Pedido} from './pedido.model';
+
+@model()
+export class Producto extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nombre: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  precio: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  imagen: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  descripcion: string;
+
+  @hasOne(() => Pedido)
+  pedido: Pedido;
+
+  constructor(data?: Partial<Producto>) {
+    super(data);
+  }
+}
+
+export interface ProductoRelations {
+  // describe navigational properties here
+}
+
+export type ProductoWithRelations = Producto & ProductoRelations;
